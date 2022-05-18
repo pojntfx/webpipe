03bcac53de59b26fc8710fcabb36bc71ef5ca07c
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-05-18 23:37:17 +0200

    feat: Add minimal CUSE CGo implementation

---
+++ cmd/wbcuse/main.go
@@ -0,0 +1,21 @@
+package main
+
+/*
+#cgo pkg-config: fuse3
+#include "cuse.h"
+*/
+import (
+	"C"
+)
+import (
+	"os"
+)
+
+func main() {
+	args := []*C.char{}
+	for _, arg := range os.Args {
+		args = append(args, C.CString(arg))
+	}
+
+	C.start(C.int(len(args)), &args[0])
+}
