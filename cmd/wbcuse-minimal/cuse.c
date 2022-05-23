ecb008955b00cbfde4fcee14fda63d4743cf6f33
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-05-23 23:57:51 +0200

    feat: add first experiments on passing Go function pointer to CGo

--- cmd/wbcuse-minimal/cuse.c
+++ cmd/wbcuse-minimal/cuse.c
@@ -1,28 +1,70 @@
 #define FUSE_USE_VERSION 31
 
+#include "cuse.h"
 #include <cuse_lowlevel.h>
+#include <fuse.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 
-void wbcuse_open(fuse_req_t req, struct fuse_file_info *fi) {
-  printf("onopen");
+void wbcuse_init(void *userdata, struct fuse_conn_info *conn) {
+  printf("wbcuse_init\n");
+  fflush(stdout);
 }
+
+void wbcuse_init_done(void *userdata) {
+  printf("wbcuse_init_done\n");
+  fflush(stdout);
+}
+
+void wbcuse_destroy(void *userdata) {
+  printf("wbcuse_destroy\n");
+  fflush(stdout);
+}
+
+// void wbcuse_open(fuse_req_t req, struct fuse_file_info *fi) {
+//   printf("wbcuse_open\n");
+//   fflush(stdout);
+// }
 
 void wbcuse_read(fuse_req_t req, size_t size, off_t off,
                  struct fuse_file_info *fi) {
-  printf("onread");
+  printf("wbcuse_read\n");
+  fflush(stdout);
 }
 
 void wbcuse_write(fuse_req_t req, const char *buf, size_t size, off_t off,
                   struct fuse_file_info *fi) {
-  printf("onwrite");
+  printf("wbcuse_write\n");
+  fflush(stdout);
+}
+
+void wbcuse_flush(fuse_req_t req, struct fuse_file_info *fi) {
+  printf("wbcuse_flush\n");
+  fflush(stdout);
+}
+
+void wbcuse_release(fuse_req_t req, struct fuse_file_info *fi) {
+  printf("wbcuse_release\n");
+  fflush(stdout);
+}
+
+void wbcuse_fsync(fuse_req_t req, int datasync, struct fuse_file_info *fi) {
+  printf("wbcuse_fsync\n");
+  fflush(stdout);
 }
 
 void wbcuse_ioctl(fuse_req_t req, int cmd, void *arg, struct fuse_file_info *fi,
-                  unsigned flags, const void *in_buf, size_t in_bufsz,
+                  unsigned int flags, const void *in_buf, size_t in_bufsz,
                   size_t out_bufsz) {
-  printf("onioctl");
+  printf("wbcuse_ioctl\n");
+  fflush(stdout);
+}
+
+void wbcuse_poll(fuse_req_t req, struct fuse_file_info *fi,
+                 struct fuse_pollhandle *ph) {
+  printf("wbcuse_pollt\n");
+  fflush(stdout);
 }
 
 int wbcuse_start(int argc, char **argv) {
@@ -38,11 +80,38 @@ int wbcuse_start(int argc, char **argv) {
 
   struct cuse_lowlevel_ops clop;
   memset(&clop, 0, sizeof(clop));
-
+  clop.init = wbcuse_init;
+  clop.init_done = wbcuse_init_done;
+  clop.destroy = wbcuse_destroy;
   clop.open = wbcuse_open;
   clop.read = wbcuse_read;
   clop.write = wbcuse_write;
+  clop.flush = wbcuse_flush;
+  clop.release = wbcuse_release;
+  clop.fsync = wbcuse_fsync;
   clop.ioctl = wbcuse_ioctl;
+  clop.poll = wbcuse_poll;
 
-  return cuse_lowlevel_main(argc, argv, &ci, &clop, NULL);
+  struct fuse_session *se;
+  int multithreaded;
+  int res;
+
+  se = cuse_lowlevel_setup(argc, argv, &ci, &clop, &multithreaded, NULL);
+  if (se == NULL) {
+    return 1;
+  }
+
+  if (multithreaded) {
+    res = fuse_session_loop_mt(se, 0);
+  } else {
+    res = fuse_session_loop(se);
+  }
+
+  cuse_lowlevel_teardown(se);
+
+  if (res == -1) {
+    return 1;
+  }
+
+  return 0;
 }
--- cmd/wbcuse-minimal/cuse.h
+++ cmd/wbcuse-minimal/cuse.h
@@ -1,1 +1,9 @@
+#define FUSE_USE_VERSION 31
+
+#include <cuse_lowlevel.h>
+
+typedef void (*closure)();
+
+typedef struct fuse_file_info fuse_file_info;
+
 int wbcuse_start(int argc, char **argv);
