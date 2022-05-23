5591ed57b317beb793d9303919c92d9617097f1c
Felicitas Pojtinger <felicitas@pojtinger.com>
2022-05-23 23:57:51 +0200

    feat: add first experiments on passing Go function pointer to CGo

--- cmd/wbcuse-minimal/main.go
+++ cmd/wbcuse-minimal/main.go
@@ -2,14 +2,23 @@ package main
 
 /*
 #cgo pkg-config: fuse3
+#cgo CFLAGS: -Wno-error=implicit-function-declaration
 #include "cuse.h"
 */
 import (
 	"C"
 )
 import (
+	"fmt"
 	"os"
 )
+
+//export wbcuse_open
+func wbcuse_open(p0 *C.fuse_req_t, p1 *C.fuse_file_info) {
+	fmt.Println("Hello, world!")
+
+	return
+}
 
 func main() {
 	args := []*C.char{}
